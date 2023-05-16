21366cb51713384a18de9db84d3b1d1eb66e4dec
Tarun <tarunraheja84@gmail.com>
2023-05-16 17:41:41 +0530

    next commit

--- follow_girls.py
+++ follow_girls.py
@@ -36,7 +36,7 @@ except:
 time.sleep(5)
 
 # Navigate to the Instagram profile URL
-url = "https://www.instagram.com/connectatpiet/followers"
+url = "https://www.instagram.com/connectatpiet/following"
 driver.get(url)
 
 # Wait for the page to load
@@ -47,6 +47,7 @@ time.sleep(3)
 
 prev_span_tags=set()
 st=set()
+prev_size=len(st)
 while True:
     span_tags = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'span.x1lliihq.x1plvlek.xryxfnj.x1n2onr6.x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x1i0vuye.xvs91rp.xo1l8bm.x1roi4f4.x10wh9bi.x1wdrske.x8viiok.x18hxmgj > span.x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft')))
     
@@ -69,8 +70,12 @@ while True:
     
 
     prev_span_tags=followers
-
-
+    curr_size=len(st)
+    if(curr_size==prev_size and curr_size>0):
+        print(f"{curr_size} followers scanned.")
+        driver.quit()
+        driver.close()
+    prev_size=curr_size
    # Get the screen size
     screen_width, screen_height = pyautogui.size()
 
@@ -85,18 +90,24 @@ while True:
     # Define a list of suffixes that indicate a female name
     female_suffixes = ['a', 'i', 'A', 'I']
 
-    # try:
-    for follower in followers:
-        first_name = follower.text.split()[0]
-        followers_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "//div[contains(@class, 'x9f619') and contains(@class, 'x1n2onr6') and contains(@class, 'x1ja2u2z') and contains(@class, 'x78zum5') and contains(@class, 'x1iyjqo2') and contains(@class, 'xs83m0k') and contains(@class, 'xeuugli') and contains(@class, 'x1qughib') and contains(@class, 'x6s0dn4') and contains(@class, 'x1a02dak') and contains(@class, 'x1q0g3np') and contains(@class, 'xdl72j9')]/span[contains(@class, 'x1lliihq') and contains(@class, 'x193iq5w') and contains(@class, 'x6ikm8r') and contains(@class, 'x10wlt62') and contains(@class, 'xlyipyv') and contains(@class, 'xuxw1ft') and contains(text(), '{}')]/following-sibling::div[contains(@class, 'x9f619') and contains(@class, 'x1n2onr6') and contains(@class, 'x1ja2u2z') and contains(@class, 'xdt5ytf') and contains(@class, 'x2lah0s') and contains(@class, 'x193iq5w') and contains(@class, 'xeuugli') and contains(@class, 'xamitd3') and contains(@class, 'x78zum5')]/button[contains(@class, '_acan') and contains(@class, '_acap') and contains(@class, '_acas') and contains(@class, '_aj1-')]".format(follower.text))))
-        for suffix in female_suffixes:
-            if first_name.endswith(suffix):
-                # followers_button.click()
-                print(first_name)
-                break
-    # except:
-    #      pyautogui.scroll(-1500)  # The negative sign indicates scrolling downwards
-    #      time.sleep(2)
+    try:
+        for follower in followers:
+            first_name = follower.text.split()[0]
+            # followers_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "followers_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'x9f619') and contains(@class, 'x1n2onr6') and contains(@class, 'x1ja2u2z') and contains(@class, 'x78zum5') and contains(@class, 'x1iyjqo2') and contains(@class, 'xs83m0k') and contains(@class, 'xeuugli') and contains(@class, 'x1qughib') and contains(@class, 'x6s0dn4') and contains(@class, 'x1a02dak') and contains(@class, 'x1q0g3np') and contains(@class, 'xdl72j9')]/span[contains(@class, 'x1lliihq') and contains(@class, 'x193iq5w') and contains(@class, 'x6ikm8r') and contains(@class, 'x10wlt62') and contains(@class, 'xlyipyv') and contains(@class, 'xuxw1ft') and contains(text(), '{}')]/following-sibling::div[contains(@class, 'x9f619') and contains(@class, 'x1n2onr6')']".format(follower.text))))
+            # followers_button = WebDriverWait(driver, 10).until(
+            # EC.presence_of_element_located((
+            #     By.XPATH, 
+            #     "//div[contains(@class, 'x9f619') and contains(@class, 'x1n2onr6') and contains(@class, 'x1ja2u2z') and contains(@class, 'x78zum5') and contains(@class, 'x1iyjqo2') and contains(@class, 'xs83m0k') and contains(@class, 'xeuugli') and contains(@class, 'x1qughib') and contains(@class, 'x6s0dn4') and contains(@class, 'x1a02dak') and contains(@class, 'x1q0g3np') and contains(@class, 'xdl72j9')]/span[contains(@class, 'x1lliihq') and contains(@class, 'x193iq5w') and contains(@class, 'x6ikm8r') and contains(@class, 'x10wlt62') and contains(@class, 'xlyipyv') and contains(@class, 'xuxw1ft') and contains(text(), {})]/following-sibling::div[contains(@class, 'x9f619') and contains(@class, 'x1n2onr6')]".format(follower.text)
+            # )))
+
+            for suffix in female_suffixes:
+                if first_name.endswith(suffix):
+                    # followers_button.click()
+                    print(first_name)
+                    break
+    except:
+        pyautogui.scroll(-1500)  # The negative sign indicates scrolling downwards
+        time.sleep(2)
 
     while(prev_span_tags==followers):
         followers = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'span.x1lliihq.x1plvlek.xryxfnj.x1n2onr6.x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x1i0vuye.xvs91rp.xo1l8bm.x1roi4f4.x10wh9bi.x1wdrske.x8viiok.x18hxmgj > span.x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft')))
